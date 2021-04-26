f93c49fbe94bea85ed5832a4d052363398c6521f
Andrey Kuksa <andrey20011005@gmail.com>
2021-04-26 08:09:41 -0700

    Initial Commit

---
+++ GesturesSecond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GesturesSecond
+//
+//  Created by Andrey Kuksa on 4/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
