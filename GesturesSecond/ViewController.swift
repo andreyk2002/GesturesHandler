4805f197cfe912d799fe7e0119ac51fd4cb9a116
Andrey Kuksa <andrey20011005@gmail.com>
2021-05-03 01:38:14 -0700

    Finished gestures app

--- GesturesSecond/ViewController.swift
+++ GesturesSecond/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gestureIndicator: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gestureIndicator.isUserInteractionEnabled = true
+        gestureIndicator.textAlignment = NSTextAlignment.center
+        gestureIndicator.numberOfLines = 2
+        gestureIndicator.backgroundColor = UIColor.yellow
     }
 
 
+    @IBAction func tap(_ sender: Any) {
+        gestureIndicator.text = "Gesture:tap\n Color: green"
+        gestureIndicator.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func pinch(_ sender: Any) {
+        gestureIndicator.text = "Gesture:pinc h\n Color: red"
+        gestureIndicator.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func rotate(_ sender: Any) {
+        gestureIndicator.text = "Gesture:rotate\n Color: blue"
+        gestureIndicator.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func swipe(_ sender: Any) {
+        gestureIndicator.text = "Gesture:swipe\n Color: grey"
+        gestureIndicator.backgroundColor = UIColor.gray
+    }
+    
+    @IBAction func longPress(_ sender: Any) {
+        gestureIndicator.text = "Gesture:longPress\n Color: orange"
+        gestureIndicator.backgroundColor = UIColor.orange
+    }
+    
 }
 
